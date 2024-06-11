24303aab7752c1f73ec1b1963191c7fb67a016d7
uo270235 <uo270235@uniovi.es>
2024-06-11 19:24:19 +0200

    base

---
+++ tfg_maria/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Editor from './components/editor';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>MOSTRADOR WEB SEMÁNTICA MOSTRADOR</h1>
+      <Editor></Editor>
+    </div>
+  );
+}
+
+export default App;
---
+++ tfg_maria/src/parser/ShexParser.js
@@ -0,0 +1,12 @@
+// Este es un ejemplo de una función de parser muy básico
+// Puedes expandir esta función con una lógica de parseo real según sea necesario
+//const shex = require("shex");
+
+const parseShex = (shexInput) => {
+    // Aquí iría la lógica real de parseo
+    // Este ejemplo simplemente convierte el input a mayúsculas
+    return shexInput;
+  };
+  
+  export default parseShex;
+  
