2324240afb0b3c7255dc29b0a390a1e388a78209
uo270235 <uo270235@uniovi.es>
2024-06-11 19:38:16 +0200

    fix build

--- tfg_maria/webpack.config.js
+++ tfg_maria/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // Tu archivo de entrada
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -17,6 +17,22 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.css$/, // Regla para archivos CSS
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/, // Regla para archivos SVG
+        use: [
+          {
+            loader: 'file-loader', // Usa file-loader para SVG
+            options: {
+              name: '[name].[ext]', // Mantiene el nombre original del archivo y su extensión
+              outputPath: 'images/' // Directorio de salida para los archivos procesados
+            }
+          }
+        ]
       }
     ]
   }
